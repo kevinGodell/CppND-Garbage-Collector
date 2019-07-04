27f48357d19cc5cb1064b8535c9182158be04fdd
Kevin Godell <kevin.godell@gmail.com>
2019-07-04 12:56:07 -0500

    add extra test

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include "gc_pointer.h"
 #include "LeakTester.h"
 
-int main() {
+void testInts() {
 
     int *ptr = new int(123);
 
@@ -35,6 +35,45 @@ int main() {
 
     // calls assignment overload Pointer = *ptr
     b = ptr2;// 4 ref to ptr, 1 ref to ptr2
+}
 
+void testIntArrs() {
+    int *ptr = new int[123];
+
+    int *ptr2 = new int[123];
+
+    // calls constructor
+    Pointer<int, 123> a(ptr);// 1 ref to ptr
+
+    // calls constructor
+    Pointer<int, 123> b(ptr);// 2 ref to ptr
+
+    // calls copy constructor
+    Pointer<int, 123> c(b);// 3 ref to ptr
+
+    // calls copy constructor
+    Pointer<int, 123> d(c);// 4 ref to ptr
+
+    std::cout << std::endl;
+
+    // calls constructor
+    Pointer<int, 123> e = new int[123];
+
+    // calls assignment overload Pointer = Pointer
+    e = d;// 5 ref to ptr
+
+    // calls assignment overload Pointer = Pointer
+    c = b;// 5 ref to ptr
+
+    // calls assignment overload Pointer = *ptr
+    b = ptr;// 5 ref to ptr
+
+    // calls assignment overload Pointer = *ptr
+    b = ptr2;// 4 ref to ptr, 1 ref to ptr2
+}
+
+int main() {
+    testInts();
+    testIntArrs();
     return 0;
 }
