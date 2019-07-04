f3475b47434a02a402fed7500226710e17e585e6
Kevin Godell <kevin.godell@gmail.com>
2019-07-04 01:07:12 -0500

    build constructor

--- gc_details.h
+++ gc_details.h
@@ -1,33 +1,27 @@
 // This class defines an element that is stored
 // in the garbage collection information list.
 //
-template <class T>
-class PtrDetails
-{
-  public:
-    unsigned refcount; // current reference count
-    T *memPtr;         // pointer to allocated memory
-    /* isArray is true if memPtr points
-to an allocated array. It is false
-otherwise. */
-    bool isArray; // true if pointing to array
-    /* If memPtr is pointing to an allocated
-array, then arraySize contains its size */
-    unsigned arraySize; // size of array
-    // Here, mPtr points to the allocated memory.
-    // If this is an array, then size specifies
-    // the size of the array.
+template<class T>
+class PtrDetails {
+public:
+    unsigned refcount;  // current reference count
+    T *memPtr;          // pointer to allocated memory
+    bool isArray;       // isArray is true if memPtr points to an allocated array. It is false otherwise.
+    unsigned arraySize; // If memPtr is pointing to an allocated array, then arraySize contains its size
 
-    PtrDetails(void)
-    {
+    PtrDetails(T *ptr, unsigned size = 0) {
         // TODO: Implement PtrDetails
+        refcount = 1;
+        memPtr = ptr;
+        isArray = size > 0;
+        arraySize = size;
     }
 };
+
 // Overloading operator== allows two class objects to be compared.
 // This is needed by the STL list class.
-template <class T>
-bool operator==(const PtrDetails<T> &ob1,
-                const PtrDetails<T> &ob2)
-{
-    // TODO: Implement operator==
+template<class T>
+bool operator==(const PtrDetails<T> &ob1, const PtrDetails<T> &ob2) {
+    // TODO-ne: Implement operator==
+    return ob1.memPtr == ob2.memPtr;// we only care that it is same memory location
 }
